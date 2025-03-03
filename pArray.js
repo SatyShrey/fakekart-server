a8e5c1c5772bbc4674e7402f63e4c81824587a3d
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-03-03 16:58:52 +0530

    Add files via upload

---
+++ pArray.js
@@ -0,0 +1,4 @@
+ 
+ export const pArray=[
+    {title:'Samsung s23 5g (8gb/256gb)',description:"Samsung s23 5g, RAM-8bg, ROM-256gb, Camera-50mp+12mp+12mp",image:"",brand:'samsung',model:'s23',price:69000}
+ ];
