6291894cb2536392a7675d1ca2e0ff36c19cbc75
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-03-03 18:09:35 +0530

    Delete pArray.js

--- pArray.js
+++
@@ -1,8 +0,0 @@
- 
- export const pArray=[
-    
-  {
-   title:'Samsung',price:59000,image:"https://th.bing.com/th/id/OIP.koqwQww2fGWbfkxgkQ0WtQHaHa?w=178&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",brand:'samsung',model:'s23'
-  }
-
- ];
