7f712a8fe8068bab97245b5cb7df51d94fa08a73
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-03-03 17:24:37 +0530

    Update pArray.js

--- pArray.js
+++ pArray.js
@@ -2,18 +2,7 @@
  export const pArray=[
     
   {
-    id: 1,
-    title:
-      "Sony WH-1000XM3 Bluetooth Wireless Over Ear Headphones with Mic (Silver)",
-    image: "https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692947383286-714WUJlhbLS._SL1500_.jpg",
-    price: 773,
-    description:
-      "Digital noise cancelling : Industry leading Active Noise Cancellation (ANC) lends a personalized, virtually soundproof experience at any situation\r\nHi-Res Audio : A built-in amplifier integrated in HD Noise Cancelling Processor QN1 realises the best-in-class signal-to-noise ratio and low distortion for portable devices.\r\nDriver Unit : Powerful 40-mm drivers with Liquid Crystal Polymer (LCP) diaphragms make the headphones perfect for handling heavy beats and can reproduce a full range of frequencies up to 40 kHz.\r\nVoice assistant : Alexa enabled (In-built) for voice access to music, information and more. Activate with a simple touch. Frequency response: 4 Hz-40,000 Hz",
-    brand: "sony",
-    model: "WH-1000XM3",
-    color: "silver",
-    category: "audio",
-    discount: 11,
+   title:'Samsung',price:59000,image:"https://th.bing.com/th/id/OIP.koqwQww2fGWbfkxgkQ0WtQHaHa?w=178&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",brand:'samsung',model:'s23'
   }
 
  ];
